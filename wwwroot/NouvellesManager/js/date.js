9ea87d223972a3679970115b99c3aa1ab56fa800
201878120 <smurfowowatch@gmail.com>
2022-10-18 17:54:08 -0400

    recompatibilized functionalities

--- wwwroot/NouvellesManager/js/date.js
+++ wwwroot/NouvellesManager/js/date.js
@@ -1,5 +1,5 @@
 function convertToFrenchDate(date) {
-    date = new Date(date);
+    date = new Date(parseInt(date));
     var options = { year: 'numeric', month: 'long', day: 'numeric' };
     var opt_weekday = { weekday: 'long' };
     var weekday = toTitleCase(date.toLocaleDateString("fr-FR", opt_weekday));
